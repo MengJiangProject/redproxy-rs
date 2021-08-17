8465e3ec292c88f3fec8de395b91cbfc9ed797f7
Bearice Ren <bearice@icybear.net>
2021-08-18 07:45:30 +0800

    parser: add comments support
    Signed-off-by: Bearice Ren <bearice@icybear.net>

--- src/rules/parser.rs
+++ src/rules/parser.rs
@@ -2,16 +2,16 @@ use std::{fmt, num::ParseIntError};
 
 use nom::{
     branch::alt,
-    bytes::{complete::tag, complete::tag_no_case},
-    character::{
-        complete::multispace0,
-        complete::{alpha1, alphanumeric1, char, digit1, hex_digit1, oct_digit1, one_of},
+    bytes::complete::{is_not, tag, tag_no_case, take_until},
+    character::complete::{
+        alpha1, alphanumeric1, char, digit1, hex_digit1, multispace0, multispace1, oct_digit1,
+        one_of,
     },
     combinator::{all_consuming, cut, map, map_opt, map_res, opt, recognize},
     error::{context, ContextError, FromExternalError, ParseError},
     multi::{many0, many1, separated_list0},
     sequence::{delimited, pair, preceded, terminated, tuple as nom_tuple},
-    AsChar, IResult, InputTakeAtPosition, Parser,
+    IResult, Parser,
 };
 
 mod string;
@@ -84,23 +84,31 @@ macro_rules! rule {
         rule!(#$($args)*);
     );
 
-    ($vis:vis $name:ident, $body:tt) => {
+    ($vis:vis $name:ident, $body:block) => {
         rule!($vis $name ( i ) -> &'a str, ctx, $body);
     };
 
-    ($vis:vis $name:ident -> $rt:ty, $body:tt) => {
+    ($vis:vis $name:ident, $ctx:ident, $body:block) => {
+        rule!($vis $name ( i ) -> &'a str, $ctx, $body);
+    };
+
+    ($vis:vis $name:ident -> $rt:ty, $body:block) => {
         rule!($vis $name ( i ) -> $rt, ctx, $body);
     };
 
-    ($vis:vis $name:ident ( $input:ident ), $body:tt) => {
+    ($vis:vis $name:ident ( $input:ident ), $body:block) => {
         rule!($vis $name ( $input ) -> &'a str, ctx, $body);
     };
 
-    ($vis:vis $name:ident ( $input:ident ) -> $rt:ty, $body:tt) => {
+    ($vis:vis $name:ident ( $input:ident ), $ctx:ident, $body:block) => {
+        rule!($vis $name ( $input ) -> &'a str, $ctx, $body);
+    };
+
+    ($vis:vis $name:ident ( $input:ident ) -> $rt:ty, $body:block) => {
         rule!($vis $name ( $input ) -> $rt, ctx, $body);
     };
 
-    ($vis:vis $name:ident ( $input:ident ) -> $rt:ty, $context:ident, $body:tt) => {
+    ($vis:vis $name:ident ( $input:ident ) -> $rt:ty, $context:ident, $body:block) => {
         #[allow(dead_code)]
         $vis fn $name<'a, E>($input: &'a str) -> IResult<&'a str, $rt, E>
         where
@@ -112,31 +120,55 @@ macro_rules! rule {
             $context!($name, $body, $input)
         }
     };
+    // ($vis:vis $name:ident ( $input:ident ) -> $rt:ty, $context:ident, $body:tt) => {
+    //     #[allow(dead_code)]
+    //     $vis fn $name<'a, O, E, F>($input: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+    //     where
+    //         E: ParseError<&'a str>
+    //             + ContextError<&'a str>
+    //             + FromExternalError<&'a str, ParseIntError>
+    //             + fmt::Debug,
+    //         F: Parser<&'a str, O, E>,
+    //     {
+    //         $context!($name, $body, $input)
+    //     }
+    // };
 }
 
 #[allow(unused_macros)]
 macro_rules! no_ctx {
-    ($name:ident, $body:tt, $input:ident) => {
-        $body
+    ($name:ident, $body:block, $input:ident) => {
+        ($body)
     };
 }
 
 #[allow(unused_macros)]
 macro_rules! ctx {
-    ($name:ident, $body:tt, $input:ident) => {
+    ($name:ident, $body:block, $input:ident) => {
         context(stringify!($name), ws($body))($input)
     };
 }
 
+rule!(eol_comment(i), no_ctx, {
+    recognize(pair(char('#'), is_not("\n\r")))(i)
+});
+rule!(inline_comment(i), no_ctx, {
+    delimited(tag("/*"), take_until("*/"), tag("*/"))(i)
+});
+rule!(blank(i), no_ctx, {
+    recognize(many0(alt((multispace1, eol_comment, inline_comment))))(i)
+});
+
 // ignore leading whitespaces
-fn ws<I, O, E, F>(f: F) -> impl FnMut(I) -> IResult<I, O, E>
+fn ws<'a, O, E, F>(f: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
 where
-    I: InputTakeAtPosition,
-    E: ParseError<I>,
-    F: Parser<I, O, E>,
-    <I as InputTakeAtPosition>::Item: AsChar + Clone,
+    E: ParseError<&'a str>
+        + ContextError<&'a str>
+        + FromExternalError<&'a str, ParseIntError>
+        + fmt::Debug,
+    F: Parser<&'a str, O, E>,
 {
-    preceded(multispace0, f)
+    preceded(blank, f)
 }
 
 rule!(string -> Value, {
@@ -435,8 +467,10 @@ mod tests {
     }
 
     macro_rules! array {
-        ($st:expr) => {
-            Value::Array(Box::new($st))
+        ($($st:expr),*) => {
+            Value::Array(Box::new(vec![
+                $($st),*
+            ]))
         };
     }
 
@@ -456,13 +490,6 @@ mod tests {
     }
 
     #[test]
-    fn op_8() {
-        let input = "a(b).c[d]";
-        let value = index!(access!(call!(vec![id!("a"), id!("b")]), id!("c")), id!("d"));
-        assert_ast(input, value);
-    }
-
-    #[test]
     fn simple_op() {
         let input = "x+1";
         let value = plus!(id!("x"), int!(1));
@@ -474,17 +501,9 @@ mod tests {
         let input = "x";
         let value = id!("x");
         assert_ast(input, value);
-    }
-
-    #[test]
-    fn tailing_spaces() {
-        let input = "          x \r\n\t        ";
+        let input = "\r\n\t x \r\n\t ";
         let value = id!("x");
         assert_ast(input, value);
-    }
-
-    #[test]
-    fn root_is_wraped_value() {
         let input = " ( ( ( ( x ) ) ) ) ";
         let value = id!("x");
         assert_ast(input, value);
@@ -495,17 +514,20 @@ mod tests {
         let input = "1 && ( 2 ) || 3 == 4";
         let value = or!(and!(int!(1), int!(2)), equal!(int!(3), int!(4)));
         assert_ast(input, value);
-    }
-
-    #[test]
-    fn paren() {
         let input = "1 && ( 2 || 3 )";
         let value = { and!(int!(1), or!(int!(2), int!(3))) };
         assert_ast(input, value);
     }
 
     #[test]
-    fn unary() {
+    fn op_8() {
+        let input = "a(b).c[d]";
+        let value = index!(access!(call!(vec![id!("a"), id!("b")]), id!("c")), id!("d"));
+        assert_ast(input, value);
+    }
+
+    #[test]
+    fn op_7() {
         let input = " ! ! ( ~ true ) ";
         let value = not!(not!(bit_not!(bool!(true))));
         assert_ast(input, value);
@@ -514,12 +536,12 @@ mod tests {
     #[test]
     fn tuple_array() {
         let input = "[ ( ) , ( ( 1 ) , ) , ( 1 , 2 ) , ( 1 , 2 , ) ]";
-        let value = array!(vec![
+        let value = array!(
             tuple!(),
             tuple!(int!(1)),
             tuple!(int!(1), int!(2)),
-            tuple!(int!(1), int!(2)),
-        ]);
+            tuple!(int!(1), int!(2))
+        );
         assert_ast(input, value);
     }
 
@@ -528,8 +550,28 @@ mod tests {
         let input = "if a then b else if c then d else e";
         let value = branch!(id!("a"), id!("b"), branch!(id!("c"), id!("d"), id!("e")));
         assert_ast(input, value);
+        let input = "if if a then b else c then d else e";
+        let value = branch!(branch!(id!("a"), id!("b"), id!("c")), id!("d"), id!("e"));
+        assert_ast(input, value);
         let input = "(a ? b : c) ? d : e";
         let value = branch!(branch!(id!("a"), id!("b"), id!("c")), id!("d"), id!("e"));
+        assert_ast(input, value);
+    }
+
+    #[test]
+    fn comments() {
+        let input = "if #comments\r\n a /* \r\n /* */then/**/b else c";
+        let value = branch!(id!("a"), id!("b"), id!("c"));
+        assert_ast(input, value);
+        let input = r#" [
+            " #not a comment ",
+            " /* also not a comment " , " */"
+        ]"#;
+        let value = array!(
+            str!(" #not a comment "),
+            str!(" /* also not a comment "),
+            str!(" */")
+        );
         assert_ast(input, value);
     }
 
@@ -539,14 +581,14 @@ mod tests {
         let value = {
             member_of!(
                 int!(1),
-                array!(vec![
+                array!(
                     str!("test"),
                     int!(1),
                     int!(2),
                     int!(3),
                     bool!(false),
                     branch!(equal!(id!("xyz"), int!(1)), int!(2), int!(3))
-                ])
+                )
             )
         };
         assert_ast(input, value);
