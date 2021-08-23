6b8c7570d301eec86844f4d9c403e729b61ecc49
Bearice Ren <bearice@icybear.net>
2021-08-23 15:50:05 +0800

    add tls support to http connector
    Signed-off-by: Bearice Ren <bearice@icybear.net>

--- src/common/tls.rs
+++ src/common/tls.rs
@@ -1,20 +1,26 @@
 use std::fs::File;
 use std::io::BufReader;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::sync::Arc;
 
 use easy_error::{err_msg, Error, ResultExt};
-use tokio_rustls::rustls::internal::pemfile::{certs, rsa_private_keys};
-use tokio_rustls::rustls::{Certificate, NoClientAuth, PrivateKey, ServerConfig};
-use tokio_rustls::TlsAcceptor;
-
 use serde::{Deserialize, Serialize};
+use tokio_rustls::rustls::{ClientConfig, ServerCertVerified, ServerCertVerifier, TLSError};
+use tokio_rustls::TlsConnector;
+use tokio_rustls::{
+    rustls::{
+        internal::pemfile::{certs, rsa_private_keys},
+        AllowAnyAnonymousOrAuthenticatedClient, AllowAnyAuthenticatedClient, Certificate,
+        ClientCertVerifier, NoClientAuth, PrivateKey, RootCertStore, ServerConfig,
+    },
+    TlsAcceptor,
+};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TlsServerConfig {
     cert: PathBuf,
     key: PathBuf,
-    client: Option<TlsClientAuthConfig>,
+    client: Option<TlsClientVerifyConfig>,
     #[serde(skip)]
     populated: Option<TlsServerConfigPopulated>,
 }
@@ -30,37 +36,49 @@ impl std::fmt::Debug for TlsServerConfigPopulated {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct TlsClientAuthConfig {
-    ca: Option<PathBuf>,
+pub struct TlsClientVerifyConfig {
+    ca: PathBuf,
     required: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct TlsClientConfig {
-    enabled: bool,
-    ca: Option<PathBuf>,
-    cert: Option<PathBuf>,
-    key: Option<PathBuf>,
-}
-
-fn load_certs(path: &Path) -> Result<Vec<Certificate>, Error> {
-    let file = File::open(path).context("failed to read certificates")?;
-    let mut reader = BufReader::new(file);
-    certs(&mut reader).map_err(|_| err_msg("load certificate"))
-}
-
-fn load_keys(path: &Path) -> Result<Vec<PrivateKey>, Error> {
-    let file = File::open(path).context("failed to read private key")?;
-    let mut reader = BufReader::new(file);
-    rsa_private_keys(&mut reader).map_err(|_| err_msg("load private key"))
+impl TlsClientVerifyConfig {
+    fn root_store(&self) -> Result<RootCertStore, Error> {
+        let mut ret = RootCertStore::empty();
+        let certs = load_certs(&self.ca)?;
+        for cert in certs {
+            ret.add(&cert).context("fail to add trusted certificate")?;
+        }
+        Ok(ret)
+    }
+    fn verifier(&self) -> Result<Arc<dyn ClientCertVerifier>, Error> {
+        let ret = if self.required {
+            AllowAnyAuthenticatedClient::new(self.root_store()?)
+        } else {
+            AllowAnyAnonymousOrAuthenticatedClient::new(self.root_store()?)
+        };
+        Ok(ret)
+    }
 }
 
 impl TlsServerConfig {
-    pub fn init(&mut self) -> Result<(), Error> {
+    fn certs(&self) -> Result<(Vec<Certificate>, PrivateKey), Error> {
         let certs = load_certs(&self.cert)?;
         let mut keys = load_keys(&self.key)?;
         let key = keys.remove(0);
-        let mut config = ServerConfig::new(NoClientAuth::new());
+        Ok((certs, key))
+    }
+
+    fn client_auth(&self) -> Result<Arc<dyn ClientCertVerifier>, Error> {
+        self.client
+            .as_ref()
+            .map(TlsClientVerifyConfig::verifier)
+            .unwrap_or_else(|| Ok(NoClientAuth::new()))
+    }
+
+    pub fn init(&mut self) -> Result<(), Error> {
+        let client_auth = self.client_auth()?;
+        let mut config = ServerConfig::new(client_auth);
+        let (certs, key) = self.certs()?;
         config
             .set_single_cert(certs, key)
             .context("failed to load certificate")?;
@@ -68,6 +86,7 @@ impl TlsServerConfig {
         self.populated = Some(TlsServerConfigPopulated { config });
         Ok(())
     }
+
     pub fn acceptor(&self) -> TlsAcceptor {
         if let Some(populated) = &self.populated {
             TlsAcceptor::from(populated.config.clone())
@@ -75,4 +94,114 @@ impl TlsServerConfig {
             panic!("TlsServerConfig not initilazed")
         }
     }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct TlsClientConfig {
+    ca: Option<PathBuf>,
+    #[serde(default)]
+    pub insecure: bool,
+    auth: Option<TlsClientAuthConfig>,
+    #[serde(skip)]
+    populated: Option<TlsClientConfigPopulated>,
+}
+
+impl TlsClientConfig {
+    fn insecure_verifier(&self) -> Arc<dyn ServerCertVerifier> {
+        struct InsecureVerifier;
+        impl ServerCertVerifier for InsecureVerifier {
+            fn verify_server_cert(
+                &self,
+                _roots: &RootCertStore,
+                _presented_certs: &[Certificate],
+                _dns_name: tokio_rustls::webpki::DNSNameRef,
+                _ocsp_response: &[u8],
+            ) -> Result<ServerCertVerified, TLSError> {
+                Ok(ServerCertVerified::assertion())
+            }
+        }
+        Arc::new(InsecureVerifier)
+    }
+    fn root_store(&self) -> Result<RootCertStore, Error> {
+        let mut ret = RootCertStore::empty();
+        let certs = self.ca.as_ref().map(load_certs).unwrap_or(Ok(vec![]))?;
+        for cert in certs {
+            ret.add(&cert).context("fail to add trusted certificate")?;
+        }
+        Ok(ret)
+    }
+
+    pub fn init(&mut self) -> Result<(), Error> {
+        let mut config = ClientConfig::new();
+        if self.ca.is_some() {
+            config.root_store = self.root_store()?;
+        }
+        if self.auth.is_some() {
+            self.auth
+                .as_ref()
+                .map(|auth| auth.setup(&mut config))
+                .unwrap()?;
+        }
+        if self.insecure {
+            config
+                .dangerous()
+                .set_certificate_verifier(self.insecure_verifier())
+        }
+        let config = Arc::new(config);
+        self.populated = Some(TlsClientConfigPopulated { config });
+        Ok(())
+    }
+
+    pub fn connector(&self) -> TlsConnector {
+        if let Some(populated) = &self.populated {
+            TlsConnector::from(populated.config.clone())
+        } else {
+            panic!("TlsClientConfig not initilazed")
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct TlsClientAuthConfig {
+    cert: PathBuf,
+    key: PathBuf,
+}
+
+impl TlsClientAuthConfig {
+    fn certs(&self) -> Result<(Vec<Certificate>, PrivateKey), Error> {
+        let certs = load_certs(&self.cert)?;
+        let mut keys = load_keys(&self.key)?;
+        let key = keys.remove(0);
+        Ok((certs, key))
+    }
+
+    fn setup(&self, config: &mut ClientConfig) -> Result<(), Error> {
+        let (certs, key) = self.certs()?;
+        config
+            .set_single_client_cert(certs, key)
+            .context("failed to load certificate")?;
+        Ok(())
+    }
+}
+
+#[derive(Clone)]
+struct TlsClientConfigPopulated {
+    config: Arc<ClientConfig>,
+}
+impl std::fmt::Debug for TlsClientConfigPopulated {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Populated")
+    }
+}
+
+fn load_certs(path: &PathBuf) -> Result<Vec<Certificate>, Error> {
+    let file = File::open(path).context("failed to read certificates")?;
+    let mut reader = BufReader::new(file);
+    certs(&mut reader).map_err(|_| err_msg("fail to load certificate"))
+}
+
+fn load_keys(path: &PathBuf) -> Result<Vec<PrivateKey>, Error> {
+    let file = File::open(path).context("failed to read private key")?;
+    let mut reader = BufReader::new(file);
+    rsa_private_keys(&mut reader).map_err(|_| err_msg("fail to load private key"))
 }
--- src/connectors/http.rs
+++ src/connectors/http.rs
@@ -5,10 +5,11 @@ use tokio::{
     io::{AsyncWriteExt, BufStream},
     net::TcpStream,
 };
+use tokio_rustls::webpki::DNSNameRef;
 
-use crate::common::copy::copy_bidi;
-use crate::common::http::HttpResponse;
+use crate::common::{copy::copy_bidi, tls::TlsClientConfig};
 use crate::context::Context;
+use crate::{common::http::HttpResponse, context::IOStream};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -16,6 +17,7 @@ pub struct HttpConnector {
     name: String,
     server: String,
     port: u16,
+    tls: Option<TlsClientConfig>,
 }
 
 pub fn from_value(value: &serde_yaml::Value) -> Result<Box<dyn super::Connector>, Error> {
@@ -30,6 +32,9 @@ impl super::Connector for HttpConnector {
     }
 
     async fn init(&mut self) -> Result<(), Error> {
+        if let Some(Err(e)) = self.tls.as_mut().map(TlsClientConfig::init) {
+            return Err(e);
+        }
         Ok(())
     }
 
@@ -37,10 +42,32 @@ impl super::Connector for HttpConnector {
         let server_addr = (self.server.to_owned(), self.port);
         let mut client = ctx.socket;
         let target = ctx.target;
+        let tls_insecure = self.tls.as_ref().map(|x| x.insecure).unwrap_or(false);
+        let tls_connector = self.tls.as_ref().map(|options| options.connector());
         tokio::spawn(async move {
             if let Err(err) = async {
                 trace!("connecting to server {:?}", server_addr);
-                let server = TcpStream::connect(server_addr).await.context("connect")?;
+                let server = TcpStream::connect(server_addr.clone())
+                    .await
+                    .context("connect")?;
+                let server: Box<dyn IOStream> = if let Some(connector) = tls_connector {
+                    let domain = DNSNameRef::try_from_ascii(server_addr.0.as_bytes())
+                        .context(&server_addr.0);
+                    let domain = if domain.is_err() && tls_insecure {
+                        DNSNameRef::try_from_ascii_str("example.com").unwrap()
+                    } else {
+                        domain?
+                    };
+                    Box::new(
+                        connector
+                            .connect(domain, server)
+                            .await
+                            .context("tls connector error")?,
+                    )
+                } else {
+                    Box::new(server)
+                };
+
                 let mut server = BufStream::new(server);
                 let request = format!("CONNECT {} HTTP/1.1\r\nHost: {}\r\n\r\n", target, target);
                 trace!("request={:?}", request);
