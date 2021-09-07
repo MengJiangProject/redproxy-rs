e16d63870fcede8db24eacc0c284f171f09b998c
Bearice Ren <bearice@icybear.net>
2021-09-07 21:56:24 +0800

    add rule stats
    Signed-off-by: Bearice Ren <bearice@icybear.net>

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -6,7 +6,13 @@ use easy_error::{Error, ResultExt};
 use log::trace;
 use serde::{Deserialize, Serialize};
 use serde_yaml::Value;
-use std::sync::Arc;
+use std::{
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
+    time::Instant,
+};
 
 use crate::{connectors::Connector, context::Context};
 
@@ -28,6 +34,18 @@ pub struct Rule {
     filter_str: Option<String>,
     #[serde(skip)]
     filter: Option<filter::Filter>,
+    #[serde(skip_deserializing)]
+    stats: RuleStatistics,
+}
+
+#[derive(Serialize, Default)]
+pub struct RuleStatistics {
+    // Total execution count
+    exec: AtomicU64,
+    // Total execution time in nanoseconds
+    time: AtomicU64,
+    // How many time rule hits (is true)
+    hits: AtomicU64,
 }
 
 impl Rule {
@@ -47,7 +65,9 @@ impl Rule {
             self.filter_str,
             self.target_name
         );
-        if self.filter.is_none() {
+        self.stats.exec.fetch_add(1, Ordering::Relaxed);
+        let t = Instant::now();
+        let ret = if self.filter.is_none() {
             true
         } else {
             match self.filter.as_ref().unwrap().evaluate(request) {
@@ -57,7 +77,14 @@ impl Rule {
                     false
                 }
             }
+        };
+        let t = t.elapsed().as_nanos() as u64;
+        self.stats.time.fetch_add(t, Ordering::Relaxed);
+        if ret {
+            self.stats.hits.fetch_add(1, Ordering::Relaxed);
         }
+        trace!("evaluation finished in {} ns", t);
+        ret
     }
 
     /// Get a reference to the rule's target name.
