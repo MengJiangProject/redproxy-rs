cf7b42680f604aa28fe5904474948dc3e0c9fc70
Bearice Ren <bearice@icybear.net>
2021-09-07 17:51:06 +0800

    update context to_string
    Signed-off-by: Bearice Ren <bearice@icybear.net>

--- src/context.rs
+++ src/context.rs
@@ -483,8 +483,12 @@ impl Display for Context {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "id={} l={}, s={}, t={}",
-            self.props.id, self.props.listener, self.props.source, self.props.target
+            "id={} l={} c={} s={} t={}",
+            self.props.id,
+            self.props.listener,
+            self.props.connector.as_deref().unwrap_or("<null>"),
+            self.props.source,
+            self.props.target
         )
     }
 }
