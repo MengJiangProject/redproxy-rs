67420062c9e1494f3e853ee4a3b760edebd2c011
Bearice Ren <bearice@icybear.net>
2022-02-26 15:55:21 +0800

    remove unnessuary callbacks
    Signed-off-by: Bearice Ren <bearice@icybear.net>

--- src/copy.rs
+++ src/copy.rs
@@ -1,6 +1,5 @@
 use crate::context::{ContextRef, ContextState, ContextStatistics};
 use easy_error::{err_msg, Error, ResultExt};
-use futures::Future;
 use std::{sync::Arc, time::Duration};
 use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, ReadHalf, WriteHalf};
 
@@ -20,7 +19,7 @@ lazy_static::lazy_static! {
     .unwrap();
 }
 
-async fn copy_stream<C, T, F>(
+async fn copy_stream<T>(
     mut r: ReadHalf<T>,
     rn: &str,
     mut w: WriteHalf<T>,
@@ -29,12 +28,9 @@ async fn copy_stream<C, T, F>(
     #[cfg(feature = "metrics")] counter: prometheus::core::GenericCounter<
         prometheus::core::AtomicU64,
     >,
-    cb: C,
 ) -> Result<(), Error>
 where
     T: AsyncRead + AsyncWrite,
-    C: FnOnce() -> F,
-    F: Future<Output = ()>,
 {
     let mut buf = [0u8; 65536];
     loop {
@@ -61,10 +57,10 @@ where
             break;
         }
     }
-    cb().await;
+
     w.shutdown()
         .await
-        .with_context(|| format!("shutdown {})", wn)) //.or(Ok(())) // Ignore "Transport endpoint is not connected"
+        .with_context(|| format!("shutdown {})", wn))
 }
 pub async fn copy_bidi(ctx: ContextRef, idle_timeout: Duration) -> Result<(), Error> {
     let ctx_lock = ctx.read().await;
@@ -87,7 +83,6 @@ pub async fn copy_bidi(ctx: ContextRef, idle_timeout: Duration) -> Result<(), Er
         client_stat.clone(),
         #[cfg(feature = "metrics")]
         IO_BYTES_CLIENT.with_label_values(&[client_label.as_str()]),
-        || async {},
     );
     let copy_s2c = copy_stream(
         sread,
@@ -97,7 +92,6 @@ pub async fn copy_bidi(ctx: ContextRef, idle_timeout: Duration) -> Result<(), Er
         server_stat.clone(),
         #[cfg(feature = "metrics")]
         IO_BYTES_SERVER.with_label_values(&[server_label.as_str()]),
-        || async {},
     );
     let interval = tokio::time::interval(Duration::from_secs(1));
     tokio::pin!(copy_c2s);
