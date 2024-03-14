f843a93c09570d8ac2a378c2cec4ec732c3b1eeb
Bearice Ren <bearice@icybear.net>
2024-03-14 17:40:36 +0900

    update deps

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, fenix, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        defaultPackage = (pkgs.makeRustPlatform {
+          inherit (fenix.packages.${system}.minimal) cargo rustc;
+        }).buildRustPackage {
+          pname = "hello";
+          version = "0.1.0";
+          src = ./.;
+          cargoSha256 = nixpkgs.lib.fakeSha256;
+        };
+      }
+    );
+}
